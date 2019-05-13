bfc0aa4512995fcef22e1cd813e57c1bbc6cd04b
Eduard Toloza <tolosaeduard@gmail.com>
2019-05-13 00:24:16 -0500

    Remove unneeded code.

--- src/functions.rs
+++ src/functions.rs
@@ -354,18 +354,8 @@ pub fn test_package(package: &str, executable: &str) {
             .args(&[&devtools_nspawn, &chroot_blackarch, executable])
             .status()
             .expect(
-                "Failed to execute the {} binary in the chroot environment, check the binary name.",
+                "Failed to execute the binary in the chroot environment, check the binary name.",
             );
-    //        if execute_package.success() {
-    //          writeln!(
-    //            coloring("green"),
-    //          "Binary {} sucessfully executed!",
-    //        executable
-    //  )
-    //            .unwrap();
-    //      } else {
-    //        writeln!(coloring("red"), "An error as ocurred while trying to execute the binary {}, are you sure that it's the binary name?", &executable).unwrap();
-    //  }
     } else {
         writeln!(
             coloring("red"),
